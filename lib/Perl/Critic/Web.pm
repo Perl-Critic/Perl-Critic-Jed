f6eefffeb37d80a64389d564b542dc12e6bea484
Jeffrey Ryan Thalhammer <jeff@thaljef.org>
2014-11-12 23:51:15 -0800

    Use PPI::HTML directly, instead of the Mojo plugin.
    This gives us more control over how things turn out.

--- lib/Perl/Critic/Web.pm
+++ lib/Perl/Critic/Web.pm
@@ -2,12 +2,12 @@ package Perl::Critic::Web;
 
 use Mojo::Base qw(Mojolicious);
 use Perl::Critic;
+use PPI::HTML;
 
 #-----------------------------------------------------------------------------
 
 sub startup {
     my $self = shift;
-    $self->plugin(PPI => {no_check_file => 1});
     $self->routes->get('/' => 'index');
     $self->routes->post('critique' => \&critique);
     return $self;
@@ -19,34 +19,19 @@ sub critique {
     my $self = shift;
     my $severity = $self->param('severity');
     my $upload = $self->param('code_file');
-    my $source_path = $upload->filename;
+    my $source_file = $upload->filename;
     my $source_code = $upload->slurp;
-    my $agent = $self->req->headers->user_agent;
 
-    my @violations = critique_source_code( $severity, \$source_code, $source_path );
-    my $status     = render_page( $self, $source_path, \$source_code, \@violations );
-}
-
-#-----------------------------------------------------------------------------
-
-sub render_page {
-    my ($self, @args) = @_;
-    my %stash;
-    @stash{ qw( filename source_code violations ) } = @args;
-    $self->render('results' => %stash);
-    return 1;
-}
-
-#-----------------------------------------------------------------------------
-
-sub critique_source_code {
-    my ($severity, $source_ref, $source_path) = @_;
+    my $doc = Perl::Critic::Document->new( -source => \$source_code, '-forced-filename' => $source_file);
     my $critic = Perl::Critic->new( -severity => $severity, -theme => 'core' );
-    my $doc = Perl::Critic::Document->new( -source => $source_ref, '-forced-filename' => $source_path);
-    my @viols = $critic->critique( $doc );
-    return @viols;
-}
+    my @violations = $critic->critique( $doc );
 
+    my $formatter = PPI::HTML->new;
+    my $source_code_html = $formatter->html( \$source_code );
+
+    my %stash = (violations  => \@violations, source_code => $source_code_html);
+    return $self->render(results => %stash);
+}
 
 #-----------------------------------------------------------------------------
 
