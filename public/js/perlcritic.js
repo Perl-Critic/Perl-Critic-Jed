6bed7f928a1dea4c63d11c1296e59567896f6f97
Jeffrey Ryan Thalhammer <jeff@thaljef.org>
2014-11-14 14:18:41 -0800

    Add diagnostic popovers.

--- public/js/perlcritic.js
+++ public/js/perlcritic.js
@@ -1,19 +1,22 @@
+$('[data-toggle="popover"]').popover();
+
 $('.analysis tr').mouseenter(function() {
     line = $(this).attr("data-src-line");
     selector = "[name=line-" + line + "]";
-    $(".ppi-code").scrollTo(selector, 150);
+    myOffset = $(this).offset().top;
+    $(".ppi-code").scrollTo(selector, myOffset - 70, 200);
     $(selector).addClass("active");
 });
 
 $('.analysis tr').mouseleave(function() {
     line = $(this).attr("data-src-line");
     selector = "[name=line-" + line + "]";
-    $(".ppi-code").scrollTo(selector, 150);
     $(selector).removeClass("active");
 });
 
 
-jQuery.fn.scrollTo = function(elem, speed) {
-    $(this).animate({scrollTop:  $(this).scrollTop() - $(this).offset().top + $(elem).offset().top - 300}, speed);
+jQuery.fn.scrollTo = function(elem, topOffset, speed) {
+    scrollAmount = $(this).scrollTop() - $(this).offset().top + $(elem).offset().top - topOffset;
+    $(this).animate({scrollTop:  scrollAmount}, speed);
     return this;
 };
