f3566baf1345087da8a37062025c484c24e861a9
Jeffrey Ryan Thalhammer <jeff@thaljef.org>
2014-11-14 21:17:50 -0800

    Add fancy submit button, redo file input.

--- public/js/perlcritic.js
+++ public/js/perlcritic.js
@@ -1,4 +1,3 @@
-$('[data-toggle="popover"]').popover();
 
 $('.analysis tr').mouseenter(function() {
     line = $(this).attr("data-src-line");
@@ -20,3 +19,27 @@ jQuery.fn.scrollTo = function(elem, topOffset, speed) {
     $(this).animate({scrollTop:  scrollAmount}, speed);
     return this;
 };
+
+//---------------------------------------------------------------------------
+// See http://www.abeautifulsite.net/whipping-file-inputs-into-shape-with-bootstrap-3
+
+$(document).on('change', '.btn-file :file', function() {
+    var input = $(this);
+    var label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [label]);
+});
+
+$(document).ready( function() {
+    $('.btn-file :file').on('fileselect', function(event, label) {
+        var input = $(this).parents('.input-group').find(':text');
+        if( input.length ) input.val(label);
+    });
+});
+
+//---------------------------------------------------------------------------
+// Popovers require activation
+
+$('[data-toggle="popover"]').popover();
+
+
+
