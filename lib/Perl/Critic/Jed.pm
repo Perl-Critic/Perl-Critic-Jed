f2c23291c799acac1177120317c7aa822e1b31fa
Jeffrey Ryan Thalhammer <jeff@thaljef.org>
2014-11-19 19:49:53 -0800

    Extract code into separate Controller class; Handle errors.

--- lib/Perl/Critic/Jed.pm
+++ lib/Perl/Critic/Jed.pm
@@ -1,52 +1,23 @@
 package Perl::Critic::Jed;
 
 use Mojo::Base qw(Mojolicious);
-use Perl::Critic;
-use PPI::HTML;
 
 #-----------------------------------------------------------------------------
 
 sub startup {
     my $self = shift;
+
     $self->moniker('pcjd');
     $self->plugin(Config => {});
+    push @{$self->routes->namespaces}, __PACKAGE__;
+
     $self->routes->get('/' => 'index');
-    $self->routes->post('critique' => \&critique);
+    $self->routes->post('critique')->to('critique#critique');
+
+    # Redirect everything else back home (no 404s)
+    $self->routes->any('/*' => sub { $_[0]->redirect_to('/') });
+
     return $self;
-}
-
-#-----------------------------------------------------------------------------
-
-sub critique {
-
-    my $c = shift; # I am a controller
-    my $severity = $c->param('severity');
-    my $upload = $c->param('upload');
-    my $source_filename = $upload->filename;
-    my $source_code_raw = $upload->slurp;
-
-    # Critique code
-    my $document = Perl::Critic::Document->new(-source => \$source_code_raw, '-forced-filename' => $source_filename);
-    my $critic = Perl::Critic->new(-severity => $severity, %{ $c->app->config->{perlcritic} || {} });
-    my $violations = [ reverse Perl::Critic::Violation::sort_by_severity($critic->critique($document)) ];
-
-    # Covert raw source code to HTML
-    my $formatter = PPI::HTML->new(line_numbers => 1);
-    my $source_code_html = $formatter->html($document->ppi_document);
-
-    # Wrap each line in a numbered <div>
-    my @lines = split /\n/, $source_code_html;
-    my $fmt = q{<div class="line" name="line-%d">%s</div>};
-    $lines[$_] = sprintf $fmt, $_+1, $lines[$_] for 0..$#lines;
-    $source_code_html = join '', @lines;
-
-    return $c->render(
-        filename    => $source_filename,
-        violations  => $violations,
-        severity    => $severity,
-        source_code => $source_code_html,
-        statistics  => $critic->statistics,
-    );
 }
 
 #-----------------------------------------------------------------------------
---
+++ lib/Perl/Critic/Jed/Critique.pm
@@ -0,0 +1,72 @@
+package Perl::Critic::Jed::Critique;
+
+use Mojo::Base 'Mojolicious::Controller';
+use Perl::Critic;
+use PPI::HTML;
+
+
+#-----------------------------------------------------------------------------
+
+sub critique {
+    my $self = shift;
+
+    return $self->_oops('Please select a severity.')
+        unless $self->param('severity');
+
+    return $self->_oops("Please select a file.")
+        unless $self->param('upload')->filename;
+
+    return $self->_oops('That file is empty. Got another one?')
+        unless $self->param('upload')->size;
+
+    return $self->_oops('Something went wrong. Was that Perl source code?')
+        unless eval { $self->_critique; 1 };
+}
+
+#-----------------------------------------------------------------------------
+
+sub _critique {
+    my $self = shift;
+
+    my $severity = $self->param('severity');
+    my $upload = $self->param('upload');
+    my $source_filename = $upload->filename;
+    my $source_code_raw = $upload->slurp;
+
+    # Critique code
+    my $document = Perl::Critic::Document->new(-source => \$source_code_raw, '-forced-filename' => $source_filename);
+    my $critic = Perl::Critic->new(-severity => $severity, %{ $self->app->config->{perlcritic} || {} });
+    my $violations = [ reverse Perl::Critic::Violation::sort_by_severity($critic->critique($document)) ];
+
+    # Covert raw source code to HTML
+    my $formatter = PPI::HTML->new(line_numbers => 1);
+    my $source_code_html = $formatter->html($document->ppi_document);
+
+    # Wrap each line in a numbered <div>
+    my @lines = split /\n/, $source_code_html;
+    my $fmt = q{<div class="line" name="line-%d">%s</div>};
+    $lines[$_] = sprintf $fmt, $_+1, $lines[$_] for 0..$#lines;
+    $source_code_html = join '', @lines;
+
+    $self->render(
+        template    => 'critique',
+        filename    => $source_filename,
+        violations  => $violations,
+        severity    => $severity,
+        source_code => $source_code_html,
+        statistics  => $critic->statistics,
+    );
+}
+
+#-----------------------------------------------------------------------------
+
+sub _oops {
+    my ($self, $alert) = @_;
+    $self->flash(alert => $alert);
+    $self->redirect_to('/');
+}
+
+#-----------------------------------------------------------------------------
+1;
+
+__END__
